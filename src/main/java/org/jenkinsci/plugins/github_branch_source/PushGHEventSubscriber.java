b23ba086fed1af3ee89e86b969dbb8054edd32ad
Dominic Scheirlinck <dominic@vendhq.com>
2016-09-03 20:33:11 +1200

    Added a pull and abstract event subscriber
    The abstract base class is to hold common logic across both subscribers.
    The pull request subscriber generates an SCMRevision, if one is
    applicable, and schedules a build from it.

--- src/main/java/org/jenkinsci/plugins/github_branch_source/PushGHEventSubscriber.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/PushGHEventSubscriber.java
@@ -28,10 +28,15 @@ import com.cloudbees.jenkins.GitHubRepositoryName;
 import hudson.Extension;
 import hudson.model.CauseAction;
 import hudson.model.Job;
+import hudson.model.TaskListener;
 import hudson.security.ACL;
+import hudson.util.LogTaskListener;
 import jenkins.branch.BranchProjectFactory;
 import jenkins.model.ParameterizedJobMixIn;
-import jenkins.scm.api.*;
+import jenkins.scm.api.SCMRevision;
+import jenkins.scm.api.SCMSource;
+import jenkins.scm.api.SCMSourceOwner;
+import jenkins.scm.api.SCMSourceOwners;
 import jenkins.util.Timer;
 import net.sf.json.JSONObject;
 import org.jenkinsci.plugins.github.extension.GHEventsSubscriber;
@@ -39,11 +44,12 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
 import org.kohsuke.github.*;
-import org.kohsuke.github.GHEventPayload.PullRequest;
+import org.kohsuke.github.GHEventPayload.Push;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -52,63 +58,65 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.google.common.collect.Sets.immutableEnumSet;
-import static org.kohsuke.github.GHEvent.PULL_REQUEST;
+import static org.kohsuke.github.GHEvent.PUSH;
 
 /**
- * This subscriber manages {@link org.kohsuke.github.GHEvent} PULL_REQUEST.
+ * This subscriber manages {@link GHEvent} PULL_REQUEST.
  */
 @Extension
-public class PullRequestGHEventSubscriber extends AbstractGHEventSubscriber {
-    private static final Logger LOGGER = Logger.getLogger(PullRequestGHEventSubscriber.class.getName());
+public class PushGHEventSubscriber extends AbstractGHEventSubscriber {
+    private static final Logger LOGGER = Logger.getLogger(PushGHEventSubscriber.class.getName());
 
     /**
      * @return set with only PULL_REQUEST event
      */
     @Override
     protected Set<GHEvent> events() {
-        return immutableEnumSet(PULL_REQUEST);
+        return immutableEnumSet(PUSH);
     }
 
     protected boolean doUpdateFromEvent(String payload, JSONObject json, WorkflowMultiBranchProject owner, GitHubSCMSource source) {
+
+        LOGGER.log(Level.INFO, "Doing update from a push event: {0}", new Object[] { payload });
+
         GitHub github;
-        GHPullRequest pull;
+        Push push;
         GHRepository repository;
-        boolean trusted;
 
         try {
             github = source.getGitHub();
-            pull = getPullRequest(payload, github).getPullRequest();
-
-            LOGGER.log(Level.INFO, "Got PR object from event payload: " + pull.getNumber());
-
-            repository = source.getRepository(github);
-            trusted = source.isTrusted(repository, pull);
+            push = getPush(payload, github);
+            repository = push.getRepository();
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "IOException during PR webhook update: " + e);
+            LOGGER.log(Level.SEVERE, "IOException during push webhook update: {0}", e);
             return false;
         }
 
-        String prHeadOwner = json.getJSONObject("pull_request").getJSONObject("head").getJSONObject("repo").getJSONObject("owner").getString("login");
-        boolean fork = !source.getRepoOwner().equals(prHeadOwner); // ?
+        String name = push.getRef();
+        String sha1 = push.getHead();
 
-        String baseHash = pull.getBase().getSha();
-        String headHash = pull.getHead().getSha();
-
-        boolean found = false;
-
-        for (boolean merge : new boolean[]{false, true}) {
-            String name = source.getPRJobName(pull.getNumber(), merge, fork);
-
-            if (name == null) {
-                continue;
-            }
-
-            PullRequestSCMHead head = new PullRequestSCMHead(pull, name, merge, trusted);
-            PullRequestSCMRevision revision = new PullRequestSCMRevision(head, baseHash, headHash);
-
-            found = found || scheduleBuild(owner, revision, name);
+        if (name.startsWith("refs/heads/")) {
+            name = name.substring("refs/heads/".length());
         }
 
-        return found;
+        Set<String> originBranchesWithPR = new HashSet<>();
+        TaskListener listener = new LogTaskListener(LOGGER, Level.INFO);
+
+        SCMRevision revision;
+
+        try {
+            revision = source.generateRevisionForBranch(name, sha1, repository, originBranchesWithPR, listener);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "IOException during push webhook generate revision: {0}", new Object[] { e });
+            return false;
+        }
+
+        if (revision == null) {
+            LOGGER.log(Level.INFO, "Skipping branch {0} -> {1} because source told us to", new Object[] { name, sha1 });
+            return false;
+        }
+
+        LOGGER.log(Level.FINE, "About to schedule build: {0} {1} {2}", new Object[] { owner.getName(), revision, name });
+        return scheduleBuild(owner, revision, name);
     }
 }
